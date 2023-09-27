5c05ecd9d36cfb9a05559af83ab7a80cfdcd1ff2
Alex Nweke <nwekealexchinedu@gmail.com>
2023-09-27 02:56:54 +0100

    add delete weather card

--- src/components/WeatherCard/WeatherCard.tsx
+++ src/components/WeatherCard/WeatherCard.tsx
@@ -1,24 +1,78 @@
-import React, { FC, useEffect } from "react";
-import { Card, CardContent, Typography } from "@material-ui/core";
-import { fetchOpenWeatherData } from "../../utils/api";
+import React, { FC, useEffect, useState } from "react";
+import {
+  Card,
+  CardContent,
+  Typography,
+  Box,
+  CardActions,
+  Button,
+} from "@material-ui/core";
+import { fetchOpenWeatherData, OpenWeatherData } from "../../utils/api";
 import "./WeatherCard.css";
 
-const WeatherCard: FC<{ city: string }> = ({ city }) => {
+type WeatherCardState = "loading" | "error" | "ready";
+
+const WeatherCardContainer: FC<{
+  children: React.ReactNode;
+  onDelete?: () => void;
+}> = ({ children, onDelete }) => {
+  return (
+    <Box mx={"4px"} my={"16px"}>
+      <Card>
+        <CardContent>{children}</CardContent>
+        {onDelete ? (
+          <CardActions>
+            <Button onClick={onDelete} color="secondary">
+              Delete
+            </Button>
+          </CardActions>
+        ) : null}
+      </Card>
+    </Box>
+  );
+};
+
+const WeatherCard: FC<{ city: string; onDelete?: () => void }> = ({
+  city,
+  onDelete,
+}) => {
+  const [weatherData, setWeatherData] = useState<OpenWeatherData | null>(null);
+  const [cardState, setCardState] = useState<WeatherCardState>("loading");
+
   useEffect(() => {
-    fetchOpenWeatherData("Nigeria")
-      .then((data) =>
-        console.log("Logging weather data", JSON.stringify(data, null, 2))
-      )
+    fetchOpenWeatherData(city)
+      .then((data) => {
+        console.log("Logging weather data", JSON.stringify(data, null, 2));
+        setWeatherData(data);
+        setCardState("ready");
+      })
       .catch((err) => {
         console.log("logging weather error", err);
+        setCardState("error");
       });
-  }, []);
+  }, [city]);
+
+  if (cardState === "loading" || cardState === "error")
+    return (
+      <WeatherCardContainer onDelete={onDelete}>
+        <Typography variant="body1">
+          {cardState === "loading"
+            ? "Loading..."
+            : "Error: could not retrive the weather condition for this city"}
+        </Typography>
+      </WeatherCardContainer>
+    );
+
   return (
-    <Card>
-      <CardContent>
-        <Typography variant="h5">{city}</Typography>
-      </CardContent>
-    </Card>
+    <WeatherCardContainer onDelete={onDelete}>
+      <Typography variant="h5">{weatherData?.name}</Typography>
+      <Typography variant="body1">
+        {Math.round(weatherData?.main?.temp)}
+      </Typography>
+      <Typography variant="body1">
+        Feels like: {Math.round(weatherData?.main?.feels_like)}
+      </Typography>
+    </WeatherCardContainer>
   );
 };
 
--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,13 +1,61 @@
-import React, { useEffect } from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
+import { Paper, InputBase, IconButton, Box, Grid } from "@material-ui/core";
+import { Add as AddIcon } from "@material-ui/icons";
+import "fontsource-roboto";
 import "./popup.css";
 import WeatherCard from "../components/WeatherCard";
 
 const App: React.FC<{}> = () => {
+  const [cities, setCites] = useState<string[]>([
+    "Nigeria",
+    "Port Harcourt",
+    "Lagos",
+    "Error",
+  ]);
+  const [cityInput, setCityInput] = useState<string>("");
+
+  const handleCityButtonClick = () => {
+    if (!cityInput) {
+      return;
+    }
+    setCites((prevCities) => [...prevCities, cityInput]);
+    setCityInput("");
+  };
+
+  const handleCityDeleteButtonClick = (index: number) => {
+    console.log("delete city", index);
+    cities?.splice(index, 1);
+    setCites([...cities]);
+  };
+
   return (
-    <div>
-      <WeatherCard city="Nigeria" />
-    </div>
+    <Box mx="8px" my="16px">
+      <Grid container>
+        <Grid item>
+          <Paper>
+            <Box px="15px" py="5px">
+              <InputBase
+                placeholder="Add a City Name"
+                value={cityInput}
+                onChange={(e) => setCityInput(e?.target?.value)}
+              />
+              <IconButton onClick={handleCityButtonClick}>
+                <AddIcon />
+              </IconButton>
+            </Box>
+          </Paper>
+        </Grid>
+      </Grid>
+      {cities?.map((city, index) => (
+        <WeatherCard
+          city={city}
+          key={`${city}-${index}`}
+          onDelete={() => handleCityDeleteButtonClick(index)}
+        />
+      ))}
+      <Box height="16px" />
+    </Box>
   );
 };
 
